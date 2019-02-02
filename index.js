d1c6472f5fbfb9dbd640970ba8f8996fc8b07a04
fortunate-paradigm-0014 <kthurman59@gmail.com>
2019-02-02 01:54:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
+//define your array here
 
 // Add your functions and code here
+function appendKittens("Ralph"){
+  var newArray = kittens.slice();
+  // or ES6 way
+  // var newArray = [...kittens];
+  newArray.push("Ralph")
+  return newArray
+}
